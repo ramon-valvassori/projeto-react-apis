4ac420299b2e50c84be5b218c2d9170cb53d74e1
ramonepico <ramonepico@gmail.com>
2023-10-06 13:35:18 -0300

    Requisição pelo GlobalState

--- objeto-react-apis/src/Pages/PokemonListPage/PokemonListPage.js
+++ objeto-react-apis/src/Pages/PokemonListPage/PokemonListPage.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { PokemonCard } from "../../Components/PokemonCard/PokemonCard";
 import { usePokemonList } from "../../hooks/useRequestPokemon";
-import { goToPokedex } from "../../Routs/coordinator";
-import { useNavigate } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import { RenderList } from "./pokemonListPageStyle";
-import axios from "axios";
+import { PokemonCard } from "../../Components/PokemonCard/PokemonCard";
+
 const PokemonListPage = () => {
+  
   const [pokemonList] = usePokemonList();
 
   const renderPokeList = pokemonList.map((pokemon) => {
-    return <PokemonCard key={pokemon.name} pokemon={pokemon}></PokemonCard>;
-  });
+    return pokemon
+  }); 
+
+console.log(renderPokeList);
 
   return (
     <>
